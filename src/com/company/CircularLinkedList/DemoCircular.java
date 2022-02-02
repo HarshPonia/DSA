c43d93eeb074f3cc14f3a2082e54455d5f449e72
Harsh Ponia <harshponia9@gmail.com>
2022-02-02 17:46:16 +0530

    Circular LinksList

--- src/com/company/CircularLinkedList/DemoCircular.java
+++ src/com/company/CircularLinkedList/DemoCircular.java
@@ -6,7 +6,7 @@ public class DemoCircular {
     Node tail;
     int size;
     Node head;
-    class Node{
+    static class Node{
         Node next;
         int data;
         Node(int d){
@@ -53,24 +53,55 @@ public class DemoCircular {
 //        tail = new_node;
         size++;
     }
-    public void InserAtPos(int d,int pos){
+    public void InserAtPos(int d,int pos) {
+//        Node new_node = new Node(d);
+//        if(isEmpty()){
+//            if(pos!= 1){
+//                System.out.println("Invalid position");
+//            }
+//            else {
+//                new_node.next = new_node;
+//                head = new_node;
+//            }
+//        }
+//        else{
+//            if(pos>size+1){
+//                System.out.println("Invalid Position ");
+//            }
+//            else{
+//                int i = 1;
+//                Node currnode = head;
+//                while(i<pos-1){
+//                    currnode = currnode.next;
+//                    i++;
+//                }
+//                new_node.next = currnode.next;
+//                currnode.next = new_node;
+//                size++;
+//            }
+//        }
         Node new_node = new Node(d);
-        if(isEmpty()){
-            if(pos!= 1){
-                System.out.println("Invalid position");
-            }
-            else {
-                new_node.next = new_node;
+        if (pos <= 0 || pos >= size) {
+            System.out.println("Invlid position : ");
+        }
+        else {
+            if (pos == 1) {
+                tail.next = new_node;
+                new_node.next = head;
                 head = new_node;
             }
-        }
-        else{
-            if(pos>size+1){
-                System.out.println("Invalid Position ");
-            }
-            else{
+            else {
 
+                Node temp = head;
+                int i = 1;
+                while (i < pos - 1) {
+                    temp = temp.next;
+                    i = i + 1;
+                }
+                new_node.next = temp.next;
+                temp.next = new_node;
             }
+            size++;
         }
     }
     public void display(){
@@ -87,15 +118,25 @@ public class DemoCircular {
 
     public static void main(String[] args) {
         DemoCircular obj = new DemoCircular();
-        Scanner sc = new Scanner(System.in);
-        int n  = sc.nextInt();
-        for(int i =0;i<n;i++){
-            obj.InsertAtEnd(sc.nextInt());
-        }
-        obj.insertAtBeg(sc.nextInt());
-        obj.display();
-        System.out.println("Length of Linked List  : " + obj.length());
+//        Scanner sc = new Scanner(System.in);
+//        int n  = sc.nextInt();
+//        for(int i =0;i<n;i++){
+//            obj.InsertAtEnd(sc.nextInt());
+//        }
+////        obj.insertAtBeg(sc.nextInt());
+////        obj.display();
+//        System.out.println("Enter the element and position : ");
+//        obj.InserAtPos(sc.nextInt(), sc.nextInt());
+//        System.out.println("Length of Linked List  : " + obj.length());
+//        obj.display();
+//        obj.insertAtBeg(10);
+        obj.InsertAtEnd(30);
+        obj.InsertAtEnd(20);
+        obj.insertAtBeg(10);
 
+        obj.InserAtPos(40 ,1);
+        obj.display();
+        System.out.println("The size of Linked List is : "+ obj.length());
     }
 
 }
